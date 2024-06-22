e2863c61c478a2cab2434fbce560787b06a3a37f
Khalied Nauly Maturino <khalidmaturino@gmail.com>
2024-06-22 15:21:30 +0700

    BEKEN

---
+++ middleware/loggedIn.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv").config();
+
+function redirectIfLoggedIn(req, res, next) {
+  const token = req.cookies.accessToken;
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+      const role = decoded.role;
+
+      switch (role) {
+        case "admin":
+          res.redirect("/admin/dashboard");
+          break;
+        case "guru":
+          res.redirect("/teacher/home");
+          break;
+        case "siswa":
+          res.redirect("/student/home");
+          break;
+        default:
+          next();
+      }
+    } catch (err) {
+      next();
+    }
+  } else {
+    next();
+  }
+}
+
+module.exports = redirectIfLoggedIn;
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
-const login = require("../controller/auth");
+const auth = require("../controller/auth");
 const siswa = require("../controller/siswa");
 const guru = require("../controller/guru");
 const admin = require("../controller/admin");
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Login' });
+router.get("/login", function (req, res, next) {
+  res.render("login", { title: "Login" });
 });
-router.get('/student/register', function(req, res, next) {
-  res.render('./student/register', { title: 'Register' });
+router.get("/student/register", function (req, res, next) {
+  res.render("./student/register", { title: "Register" });
 });
-router.get('/teacher/register', function(req, res, next) {
-  res.render('./teacher/register', { title: 'Register' });
+router.get("/teacher/register", function (req, res, next) {
+  res.render("./teacher/register", { title: "Register" });
 });
+router.post("/login", auth.login);
+router.post("/studentRegis", auth.registerSiswa);
+router.post("/teacherRegis", auth.registerTeacher);
 
 module.exports = router;
