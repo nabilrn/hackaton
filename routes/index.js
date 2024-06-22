d55243fc21722c2dde6b362cae4d760c4d5b7cb9
nabil260904 <116415990+nabil260904@users.noreply.github.com>
2024-06-22 14:30:29 +0700

    landing page regis student teacher

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+router.get('/student/register', function(req, res, next) {
+  res.render('./student/register', { title: 'Register' });
+});
+router.get('/teacher/register', function(req, res, next) {
+  res.render('./teacher/register', { title: 'Register' });
+});
 
 module.exports = router;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/*.{html,js,ejs}"],
+  content: [
+    "./views/*.{html,js,ejs}",
+    "./views/admin/*.{html,js,ejs}",
+    "./views/student/*.{html,js,ejs}",
+    "./views/teacher/*.{html,js,ejs}",
+    "node_modules/preline/dist/*.js",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [require("preline/plugin")],
 };
